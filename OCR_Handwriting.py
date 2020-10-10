da60ef5a20824a0b8808f9e504b771a5ebf352f9
Derek Badner <derek_badner@hotmail.com>
2020-10-10 13:17:54 -0700

    implemented keyWord probability based detection in OCR_Handwriting.py

--- OCR_Handwriting.py
+++ OCR_Handwriting.py
@@ -55,7 +55,8 @@ class FindCharsWords:
 		# Applied dilation
 		kernel3 = cv2.getStructuringElement(cv2.MORPH_RECT, (2, 2))
 		gray = cv2.morphologyEx(gray, cv2.MORPH_ERODE, kernel3)
-		#cv2.imshow("Preprocessed", gray)
+		grayS = self.ResizeImage(gray, 800, 800)
+		#cv2.imshow("Preprocessed", grayS)
 		#cv2.waitKey(0)
 		return gray
 
@@ -156,7 +157,7 @@ class FindCharsWords:
 				(xW, yW, wW, hW) = word[0] #read in word dimensions
 				#compare to determine whether character is part of current word
 				xDif = x - (xW+wW) #check x
-				if xDif < hW: #compare to word height
+				if xDif < hW/1.3: #compare to word height
 					#check y-overlap
 					if not(y > (yW + hW) or (y + h) < yW):#need to also check amount of overlap
 						ovl = (min(y+h,yW+hW) - max(y,yW)) / (max(y+h,yW+hW) - min(y,yW)) #percentage overlap
@@ -174,9 +175,140 @@ class FindCharsWords:
 								break  # exit for loop
 			if not(fndWord): #start a new word
 				wordList.append((char[1],[i])) #add dimensions of first character and charID to the wordlist
-		# final loop to throw out chars that are not part of words (i.e. wordList entries with only 1 char)
+		# final loop to throw out words that only have one character
+		for i in range(len(wordList)-1,-1,-1):
+			words = wordList[i]
+			if len(words[1]) < 2:
+				del wordList[i]
 
 		return chars, wordList
+
+	def RunModel(self, chars, wordList, gray, image):
+		#run the model to predict characters
+
+		# function level variables
+		# define the list of label names
+		imgAnno = image.copy() #make a local copy of coloured image
+		labelNames = "0123456789"
+		labelNames += "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+		labelNames = [l for l in labelNames]
+		probNumList = [] #list corresponding to wordList indices with probability of number
+		wordCharList = [] #list of character arrays in words corresponding to wordList
+		keyWordList = [] #list of char lists defining keywords corresponding to wordList
+		keyWordList.append(['F','R','O','M'])
+		keyWordList.append(['T','O'])
+		keyWordList.append(['D','R','Y'])
+		keyWordList.append(['W','E','T'])
+		maxProbKeyWordList = []
+		maxProbKeyWordListInd = [] #corresponding indices
+		keyWordListInd = []
+		#maxProbKeyWordList = np.empty(len(keyWordList), dtype=float)  # list of length len(keyWordList) x 2 containing ID of max probability word for each keyword, and corresponding probability
+		#keyWordListInd = np.empty(len(keyWordList), dtype=list) #corresponding indices for keyWordList, values from 0 to 35
+		for keyWord in keyWordList:
+			keyWordInd = []
+			for char in keyWord:
+				for i in range(len(labelNames)):
+					l = labelNames[i]
+					if l == char:
+						keyWordInd.append(i)
+						break
+			keyWordListInd.append(keyWordInd)
+			maxProbKeyWordList.append(0) #initialize list of max probabilities to 0
+			maxProbKeyWordListInd.append(-1) #initialize index to -1
+
+		# extract the bounding box locations and padded characters
+		boxes = [b[1] for b in chars]
+		chars = np.array([c[0] for c in chars], dtype="float32")
+		# OCR the characters using our handwriting recognition model
+		preds = self.Model.predict(chars)
+
+
+		# loop over the predictions and bounding box locations together
+		for (pred, (x, y, w, h)) in zip(preds, boxes):
+			# for (x, y, w, h) in boxes:
+			# find the index of the label with the largest corresponding
+			# probability, then extract the probability and label
+			i = np.argmax(pred)
+			prob = pred[i]
+			label = labelNames[i]
+			# draw the prediction on the image
+			#print("[INFO] {} - {:.2f}%".format(label, prob * 100))
+			cv2.rectangle(imgAnno, (x, y), (x + w, y + h), (0, 255, 0), 2)
+			cv2.putText(imgAnno, label, (x - 10, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 1.2, (0, 0, 255), 2)
+		# loop over words
+		for wInd in range(len(wordList)):
+			words = wordList[wInd]
+			#if len(words[1]) > 1: #show wordBoxes if > 1 character
+			(x, y, w, h) = words[0]
+			cv2.rectangle(imgAnno, (x, y), (x + w, y + h), (255, 51, 204), 2) #rectangle around word
+
+		# show the image
+		imageS = self.ResizeImage(imgAnno, 800, 800)
+		cv2.imshow("Image", imageS)
+		#grayS = self.ResizeImage(gray, 800, 800)
+		#cv2.imshow("Gray", grayS)
+		cv2.waitKey(0)
+
+		#loop over words again
+		for wInd in range(len(wordList)):
+			words = wordList[wInd]
+			probNum = 0 #probability that word contains a number
+			wordChars = []  # list of characters in word
+			#loop over characters in word and determine probability of #,
+			for i in words[1]: #for each ith character
+				prob = preds[i]
+				for j in range(10): #loop through probability of number characters
+					probNum += prob[j]
+				#probNum /= 10
+				wordChars.append(labelNames[np.argmax(preds[i])]) #store the max likelihood character in wordCharList
+				# loop through keywords
+
+
+			probNum /= len(words[1])
+			probNumList.append(probNum)
+			wordCharList.append(wordChars)
+			#else: #length of word = 1
+				#probNumList.append(-1) #null val
+
+			#check probability of being a keyword
+			for k in range(len(keyWordListInd)):
+				keyWordInd = keyWordListInd[k]
+				probKeyWord = 0
+				if len(words[1]) == len(keyWordInd):  # same length, so check probability
+					for i in range(len(keyWordInd)):  # for each ith character
+						ind = words[1][i]
+						probKeyWord += preds[ind][keyWordInd[i]] #find probability that ith characters match
+					probKeyWord /= len(keyWordInd)
+					#now compare to list, and replace if the new highest likelihool of word is found
+					if probKeyWord > maxProbKeyWordList[k]: #new highest likelihood found
+						maxProbKeyWordList[k] = probKeyWord
+						maxProbKeyWordListInd[k] = wInd  # corresponding word index
+		#show image of keyWords picked out, and print probabilities correct
+		imgKeyWords = image.copy()
+		for k in range(len(maxProbKeyWordListInd)):
+			temp: str = ""
+			for c in keyWordList[k]:
+				temp = temp + c
+			temp = temp + ": P={:.1f}%".format(maxProbKeyWordList[k]*100)
+			print(temp)
+			execute = True
+			if maxProbKeyWordList[k] < 0.4: #keyword not found with sufficient probability (40%)
+				execute = False
+			if k == 2 and maxProbKeyWordList[2] < maxProbKeyWordList[3]:#compare probability of dry vs wet, only show the higher probability
+				execute = False
+			elif k == 3 and maxProbKeyWordList[3] < maxProbKeyWordList[2]:
+				execute = False
+			if execute:
+				(x, y, w, h) = wordList[maxProbKeyWordListInd[k]][0]
+				cv2.rectangle(imgKeyWords, (x, y), (x + w, y + h), (0, 255, 0), 2)
+				for i in range(len(keyWordListInd[k])): #characters in keyword
+					label = keyWordList[k][i]
+					charInd = wordList[maxProbKeyWordListInd[k]][1][i]
+					(xC, yC, wC, hC) = boxes[charInd]
+					cv2.putText(imgKeyWords, label, (xC - 10, yC - 10), cv2.FONT_HERSHEY_SIMPLEX, 1.2, (0, 0, 255), 2)
+		imageS = self.ResizeImage(imgKeyWords, 800, 800)
+		cv2.imshow("Keywords Image", imageS)
+		cv2.waitKey(0)
 
 	def OCRHandwriting(self):
 		for image_file in os.listdir(self.InputDir):
@@ -187,37 +319,6 @@ class FindCharsWords:
 				image = self.ResizeImage(image, 2000, 2000)
 				gray = self.Preprocess(image) #preprocess the image, convert to gray
 				chars, wordList = self.FindCharsWords(gray) #find characters and words, store as [image, (x, y, w, h)]
+				self.RunModel(chars, wordList, gray, image) #run the model to predict characters
 
-				# extract the bounding box locations and padded characters
-				boxes = [b[1] for b in chars]
-				chars = np.array([c[0] for c in chars], dtype="float32")
-				# OCR the characters using our handwriting recognition model
-				preds = self.Model.predict(chars)
-				# define the list of label names
-				labelNames = "0123456789"
-				labelNames += "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-				labelNames = [l for l in labelNames]
 
-				# loop over the predictions and bounding box locations together
-				for (pred, (x, y, w, h)) in zip(preds, boxes):
-					# for (x, y, w, h) in boxes:
-					# find the index of the label with the largest corresponding
-					# probability, then extract the probability and label
-					i = np.argmax(pred)
-					prob = pred[i]
-					label = labelNames[i]
-					# draw the prediction on the image
-					print("[INFO] {} - {:.2f}%".format(label, prob * 100))
-					cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
-					cv2.putText(image, label, (x - 10, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 1.2, (0, 0, 255), 2)
-				#loop over words and show wordBoxes if > 1 character
-				for words in wordList:
-					if len(words[1])>1:
-						(x,y,w,h) = words[0]
-						cv2.rectangle(image, (x, y), (x + w, y + h), (255, 51, 204), 2)
-				# show the image
-				imageS = self.ResizeImage(image, 800, 800)
-				cv2.imshow("Image", imageS)
-				grayS = self.ResizeImage(gray, 800, 800)
-				cv2.imshow("Gray", grayS)
-				cv2.waitKey(0)
